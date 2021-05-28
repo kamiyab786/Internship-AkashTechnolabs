07c879035e62ea60a89e001a960291f834d6fc09
kamiyab786 <50388943+kamiyab786@users.noreply.github.com>
2021-05-28 13:31:10 +0530

    Delete Task3 directory

--- Task3/pract_11_+ve_-ve_nested.py
+++
@@ -1,9 +0,0 @@
-for i in range(3):
-    number = int(input(f'Enter the number: '))
-    if number >= 0:
-        if number == 0:
-            print(f"{number} is Zero.")
-        else:
-            print(f"{number} is Positive.")
-    else:
-        print(f"{number} is Negative.")
--- Task3/pract_15_start-end.py
+++
@@ -1,5 +0,0 @@
-start = int(input(f'Enter the starting number: '))
-end = int(input(f'Enter the ending number: '))
-
-for i in range(start, end-1, -3):
-    print(i)
