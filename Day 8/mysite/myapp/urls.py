e960b31213c6f3568a82c8b4de2d7a97be33636f
kamiyab786 <kamiyabhusain.25@gmail.com>
2021-06-06 20:38:27 +0530

    Done

--- Day 8/mysite/myapp/urls.py
+++ Day 8/mysite/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.travel, name='travel'),
+    path('travel', views.travel, name='travel'),
     path('services', views.services, name='services'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
